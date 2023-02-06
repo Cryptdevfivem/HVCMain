dbe31a1397715e872446e1305919b4307adfbdec
HVC <118839660+HVCFivem@users.noreply.github.com>
2023-02-06 11:52:38 -0800

    add(veh): part 1

--- [FiveM]/[system]/hardcap/server.lua
+++ [FiveM]/[system]/hardcap/server.lua
@@ -18,7 +18,7 @@ AddEventHandler('playerDropped', function()
 end)
 
 AddEventHandler('playerConnecting', function(name, setReason)
-  local cv = GetConvarInt('sv_maxclients', 128)
+  local cv = GetConvarInt('sv_maxclients', 32)
 
   print('Connecting: ' .. name .. '^7')
 
