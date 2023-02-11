781e76636e6f6bc0fd14a810b0c58f7826507167
HVC-FiveM <118839660+HVCFivem@users.noreply.github.com>
2023-02-11 02:26:18 +0000

    Revert "-(revert)"
    This reverts commit fd1b2a7f47c77bd9a9134c4ab79a7a1b1f16ced5.

--- [HVC]/HVCRageUIMenus/client/cl_mpdclockon.lua
+++ [HVC]/HVCRageUIMenus/client/cl_mpdclockon.lua
@@ -15,19 +15,6 @@ Citizen.CreateThread(function()
     end
 end)
 
-Citizen.CreateThread(function()
-    while true do 
-        Citizen.CreateThread(function() 
-            while true do
-                for k, v in pairs(cfgChngeIdentity.coords) do 
-                    local x,y,z = table.unpack(v.marker)
-                    local v1 = vector3(x,y,z)
-                    if isInArea(v1, 100.0) then 
-                        DrawMarker(25, v1.x,v1.y,v1.z - 0.888888, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 2.5, 255, 0, 0, 150, 0, 0, 2, 0, 0, 0, false)
-                    end
-                end)
-            end
-            
 local MenuOpen = false;
 local inMarker = false;
 Citizen.CreateThread(function()
